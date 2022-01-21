a74c3a1f8c640c8148bed343b3be63984c56d0de
Matheus Castro <MATHEUSCASTRO161096@gmail.com>
2022-01-21 00:38:33 -0300

    Criado o controller e a classe de validação com fluentApi.

---
+++ CrudPessoaFisicaApi/Controllers/PessoaFisicaController.cs
@@ -0,0 +1,65 @@
+﻿using CrudPessoaFisicaApi.Application.IApplication;
+using CrudPessoaFisicaApi.Domain.Entities;
+using CrudPessoaFisicaApi.Domain.Validator;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CrudPessoaFisicaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaFisicaController : ControllerBase
+    {
+        private readonly IPessoaFisicaApplication _pessoaFisicaApplication;
+        public readonly PessoaFisicaValidator _validationRules = new PessoaFisicaValidator();
+        public ValidationResult _result = new ValidationResult();
+
+        public PessoaFisicaController(IPessoaFisicaApplication pessoaFisicaApplication)
+        {
+            _pessoaFisicaApplication = pessoaFisicaApplication;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PessoaFisica pessoaFisica)
+        {
+            try
+            {
+                _result = _validationRules.Validate(pessoaFisica);
+                if (_result.IsValid)
+                {
+                    return Ok();
+                }
+                return BadRequest(_result.Errors);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CrudPessoaFisicaApi/Domain/Validator/PessoaFisicaValidator.cs
@@ -0,0 +1,30 @@
+﻿using CrudPessoaFisicaApi.Domain.Entities;
+using FluentValidation;
+
+namespace CrudPessoaFisicaApi.Domain.Validator
+{
+    public class PessoaFisicaValidator : AbstractValidator<PessoaFisica>
+    {
+        const string ERRO_CAMPO_VAZIO = " não pode ser vazio";
+        const string ERRO_CAMPO_NULL = " não pode ser nulo";
+        const string ERRO_DATA_INVALIDA = " Data inválida";
+
+
+        public PessoaFisicaValidator()
+        {
+            RuleFor(x => x.NomeCompleto).NotNull().WithMessage(ERRO_CAMPO_NULL)
+                .NotEmpty().WithMessage(ERRO_CAMPO_VAZIO)
+                .MaximumLength(200).WithMessage("Tamanho maximo do nome é {MaxLength}");
+
+            RuleFor(x => x.Cpf).NotNull().WithMessage(ERRO_CAMPO_NULL)
+                .NotEmpty().WithMessage(ERRO_CAMPO_VAZIO);
+
+            RuleFor(x => x.DataNascimento).Must(ValidaData).WithMessage(ERRO_DATA_INVALIDA);
+        }
+
+        private bool ValidaData(DateTime date)
+        {
+            return !date.Equals(default(DateTime));
+        }
+    }
+}
