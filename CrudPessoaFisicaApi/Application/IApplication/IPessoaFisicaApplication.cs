8d40893d997833a105932b809032eac878a050ea
Matheus Castro <MATHEUSCASTRO161096@gmail.com>
2022-01-20 23:09:02 -0300

    Adicionada a camada de aplicação.

---
+++ CrudPessoaFisicaApi/Application/IApplication/IPessoaFisicaApplication.cs
@@ -0,0 +1,17 @@
+﻿using CrudPessoaFisicaApi.Domain.Entities;
+
+namespace CrudPessoaFisicaApi.Application.IApplication
+{
+    public interface IPessoaFisicaApplication
+    {
+        PessoaFisica Get(int id);
+
+        List<PessoaFisica> GetAll();
+
+        bool Post(PessoaFisica pessoaFisica);
+
+        bool Put(PessoaFisica pessoaFisica);
+
+        bool Delete(int id);
+    }
+}
---
+++ CrudPessoaFisicaApi/Application/PessoaFisicaApplication.cs
@@ -0,0 +1,33 @@
+﻿using CrudPessoaFisicaApi.Application.IApplication;
+using CrudPessoaFisicaApi.Domain.Entities;
+
+namespace CrudPessoaFisicaApi.Application
+{
+    public class PessoaFisicaApplication : IPessoaFisicaApplication
+    {
+        public PessoaFisica Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PessoaFisica> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Post(PessoaFisica pessoaFisica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Put(PessoaFisica pessoaFisica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
